d8186bd68bacd87a8eeb0ec5d8aaf3c2ffefb04e
LudivineCo <91370373+LudivineCo@users.noreply.github.com>
2022-03-21 11:43:06 +0000

    Create Traffic_Lights.s

---
+++ Traffic_Lights.s
@@ -0,0 +1,84 @@
+#include <xc.inc>
+    
+global  Traffic_lights_setup,Traffic_Lights_Light_pattern,After_button_press
+extrn   LCD_delay_ms,one_sec_delay
+extrn   Button, Other_Button,check_photodiode
+
+psect	udata_acs   ; reserve data space in access ram
+;test:    ds 1    ; reserve one byte for a test variable    
+    
+psect	Traffic_Lights_code,class=CODE 
+    
+Traffic_lights_setup:;set up ports
+    movlw   0x00
+    movwf   TRISB, A ;output- for buzzer
+    movlw   0x00
+    movwf   TRISD, A ;output- for traffic lights
+    movlw   0x00
+    movwf   TRISH, A  ;Setting the pedestrian crossing to be an output
+    movlw   0x03
+    movwf   TRISE, A ;pins 1 and 2 input, 3 output- external buttons and button lights
+    movlb   0xF
+    bsf     REPU
+    clrf    LATE
+;    
+;    movlw   0xFF
+;    movwf   PR2 ;set the period
+;    movlw   0xFF
+;    movwf   CCPR4L;set duty cycle
+;    movlw   0xFF
+;    movwf   CCP4CON;extra duty cycle bits
+;    clrf    TRISC,A; clear approprite tris bits
+;    clrf    TRISB,A
+;    movlw   0xFF; set prescaler
+;    movwf   TMR2
+;    movlw   0xFF; enable timer2
+;    movwf   T2CON
+    ;configure CCp4 module
+        
+    return
+Traffic_Lights_Light_pattern:;standard traffic light sequence
+    movlw 0x04
+    movwf LATH, A    ;Setting the pedestrian crossing to red as default
+    movlw  0x09;red
+    movwf  LATD, A
+    call   delay 
+red_orange:
+    movlw  0x1B; red and orange
+    movwf  LATD, A
+    call   delay
+    movlw  0x24; green
+    movwf  LATD, A
+    call   delay
+    movlw  0x12; orange
+    movwf  LATD, A
+    call   delay     
+return
+    
+After_button_press:; in oder to synchronise back to default pattern 
+    goto red_orange         ;after the crossing 
+    return    
+
+delay:	
+        call    check_photodiode
+	movlw	0x23 ;set length of delay
+	movwf	0x01
+	movlw	0xC2
+	movwf	0x02
+	movlw	0x46
+	movwf	0x03
+Delay_0:
+        call    Button
+	call    Other_Button
+	decfsz	0x01, f ;loop until zero
+	goto	loop1
+	decfsz	0x02, f
+loop1:	goto	loop2
+	decfsz	0x03, f
+loop2:	goto	Delay_0			;2 cycles
+	;goto	$+1			;4 cycles (including call)
+	return
+    
+end    
+
+
